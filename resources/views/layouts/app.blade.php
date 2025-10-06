fda5b186cce78a7ccd4f75e8f59f3501a72eec1e
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-06 15:00:16 -0700

    update landing page and layout files

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="scroll-smooth">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <title>Jeffrey Zeller | Freelance Web Developer</title>
+    <style>
+        .active {
+            background: color-mix(in oklab, var(--color-gray-900) 20%, transparent);
+        };
+    </style>
+    </head>
+    <body>
+        @include('layouts.nav')
+
+        @yield('content')
+
+        @include('layouts.footer')
+        <script src="https://elfsightcdn.com/platform.js" async></script>
+        <div class="elfsight-app-708553fe-5e5d-4f26-92bd-4a1f5833ea13" data-elfsight-app-lazy></div>
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    </body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,31 @@
+<!-- Navigation -->
+<nav class="bg-slate-900/98 border-gray-200 dark:bg-gray-900 absolute sticky top-0 z-45">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://www.jeffreyzeller.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <!-- <img src="#" class="h-8" alt="Jeffrey Zeller Logo" /> -->
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-mono text-white">jeffreyzeller.com</span>
+        </a>
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-slate-700 md:flex-row md:space-x-1 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white/10 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li class="px-3 py-2">
+            <a href="#home"  class="block text-neutral-50 rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
+            </li>
+            <li class="px-3 py-2">
+            <a href="#services" class="block text-neutral-50 rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+            </li>
+            <li class="px-3 py-2">
+            <a href="#portfolio" class="block text-neutral-50 rounded-sm  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Portfolio</a>
+            </li>
+            <li class="px-3 py-2">
+            <a href="#contact" class="block text-neutral-50 rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
