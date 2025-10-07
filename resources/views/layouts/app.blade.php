039084a1a73b45041d9289de40b72c443952b14a
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-07 04:10:30 -0700

    update landing page for mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         @include('layouts.nav')
-        <div class="w-full bg-[url({{ asset('images/stage.jpg') }})] bg-fixed bg-bottom bg-cover transform-gpu min-h-[calc(100vh-72px)] overflow-y-scroll snap-y snap-mandatory fixed top-[72] left-0">
+        <div class="w-full bg-[url({{ asset('images/stage.jpg') }})] bg-fixed bg-bottom bg-cover transform-gpu h-[calc(100vh)] overflow-y-scroll snap-y snap-mandatory fixed top-[72] left-0">
         @yield('content')
         </div>
         @include('layouts.footer')
