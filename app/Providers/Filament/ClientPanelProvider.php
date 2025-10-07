620d6ff6fe2b9eb45c520d891a59398aa2b8de82
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-06 18:03:57 -0700

    update client panel provider for login

--- app/Providers/Filament/ClientPanelProvider.php
+++ app/Providers/Filament/ClientPanelProvider.php
@@ -27,7 +27,7 @@ class ClientPanelProvider extends PanelProvider
             ->id('client')
             ->path('')
             ->domain('client.jeffreyzeller.com')
-            ->login(Login::class)
+            ->login()
             ->colors([
                 'primary' => Color::Amber,
             ])
