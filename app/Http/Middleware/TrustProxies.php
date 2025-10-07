282c98fdafa61490b193022450fa1657c1c711bd
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-06 19:35:49 -0700

    remove trustedproxies didnt work

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,20 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 class TrustProxies
 {
     /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string|null
-     */
-    protected $proxies = '*';
-
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
-    
-    /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
