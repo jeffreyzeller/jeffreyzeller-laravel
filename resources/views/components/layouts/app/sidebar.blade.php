cb465c62a60eed5607f7c16741db1e238cc1d414
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-06 19:00:47 -0700

    update dashboard sidebar

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,7 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                    <flux:navlist.item icon="home" :href="route('homepage')" :current="request()->routeIs('homepage')" wire:navigate>{{ __('Homepage') }}</flux:navlist.item>
+                    <flux:navlist.item icon="home" :href="route('home')" :current="request()->routeIs('home')" wire:navigate>{{ __('Homepage') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
             
