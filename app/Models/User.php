e21999f3f3c2367ed36c6c2a8ff0d14e62bd238f
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-06 19:02:48 -0700

    update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -61,14 +59,5 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        if ($panel->getId() === 'client') {
-            return true;
-        }
-
-        return false;
     }
 }
