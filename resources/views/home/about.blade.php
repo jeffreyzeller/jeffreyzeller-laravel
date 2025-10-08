c7ddf9ff81c94fd9bc2720d211fc10ad3a7983ee
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-08 06:18:03 -0700

    update about section

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,11 +1,12 @@
 <section id="about" class="w-full text-center content-center bg-white/80 dark:bg-slate-950/80 snap-start min-h-[calc(100vh)] bg-[url({{ asset('images/bg-wave-trans-flipped.svg') }})] dark:bg-[url({{ asset('images/bg-wave-dark-trans-flipped.svg') }})] bg-cover bg-no-repeat bg-top pt-75">
     <h2 class="text-4xl text-gray-900 dark:text-gray-100 font-bold mb-5">About Me</h2>
     <div class="max-w-5xl mx-auto flex flex-col sm:flex-row p-5">
-        <div class="flex-1 p-5 bg-white/80 dark:bg-gray-900 text-gray-900 dark:text-gray-100 text-left rounded ">
-            <p class="mb-2">I’m a Laravel App Developer with over 5 years of experience crafting custom web applications for clients.<p>
-            <p class="mb-2">I focus on clean, functional UI design using TailwindCSS and Filament, following meticulous design and usability principles.</p>
-            <p class="mb-2">By keeping third-party dependencies to a minimum, I ensure each application is secure, stable, and built entirely in-house for long-term reliability.</p>
-            
+        <div class="flex-1 p-5 bg-white/100 shadow shadow-lg mr-5 dark:bg-gray-900 text-gray-900 dark:text-gray-100 text-left rounded-xl outline-blue-500/50 outline-4 outline-offset-5">
+            <p class="mb-2">I’m a <strong>Laravel App Developer</strong> with over <strong>5 years of experience</strong> building custom web applications that help businesses run smoother.<p>
+            <p class="mb-2">Too many tools force you into a box. I believe your software should fit <i>you</i>. That’s why I design with precision using <strong>TailwindCSS</strong> and <strong>Filament</strong>, keeping UIs clean, intuitive, and performance-driven.</p>
+            <p class="mb-2">I deliberately limit third-party dependencies — every feature is built in-house. This means fewer security risks, greater stability, and cleaner, more maintainable code in the long run.</p>
+            <p class="mb-2">When we work together, you won’t get a templated site—you’ll get a system built precisely for <i>how you operate</i>.</p>
+            <a href="#portfolio" class="text-blue-500 hover:underline">See My Work →</a><br />
             <a href="#contact" 
             class="mt-2 inline-block px-8 py-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-xl shadow-lg shadow-blue-600/30 transition">
                 <x-heroicon-s-chat-bubble-left-right class=" inline w-4 mr-1" /> Let’s Me Help You Build Your Website
