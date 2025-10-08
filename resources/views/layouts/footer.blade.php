60990f6d1faa04fbce046f33d47d9a2b935d2f00
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-08 01:15:06 -0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="relative h-40 max-w-screen bg-white dark:bg-gray-800 snap-end bg-[url({{ asset('images/bg-wave-trans-flipped.svg') }})] dark:bg-[url({{ asset('images/bg-wave-dark-trans-flipped.svg') }})] bg-cover bg-no-repeat bg-top">   
+<footer class="relative h-25 lg:h-50 max-w-screen bg-white dark:bg-gray-700 snap-end bg-[url({{ asset('images/bg-wave-trans-flipped.svg') }})] dark:bg-[url({{ asset('images/bg-wave-dark-trans-flipped.svg') }})] bg-cover bg-no-repeat bg-top">   
     <div class="absolute w-[100%] bottom-0 flex content-center">
-        <div class="w-[100%] mx-auto text-center p-5">
+        <div class="w-[100%] mx-auto text-left text-xl text-gray-300 dark:text-gray-500 p-5">
             &copy; jeffreyzeller.com
         </div>
     </div>
