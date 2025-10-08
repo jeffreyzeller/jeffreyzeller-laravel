c3a762f4d546227afce07987bf283799790d7ee8
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-08 07:49:59 -0700

    fix spacing

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -1,6 +1,5 @@
 <section id="portfolio" class="w-full text-center content-center bg-blue-700 dark:bg-gray-800 snap-start min-h-[calc(100vh-300px)] pt-28">
-    <h2 class="text-4xl text-white dark:text-gray-100 font-bold">My Recent Work</h2>    
-    <span class="md:hidden text-white dark:gray-100">Swipe right to view more</span>            
+    <h2 class="text-4xl text-white dark:text-gray-100 font-bold mb-5">My Recent Work</h2>         
     
     <div class="lg:max-w-5xl mx-5 sm:mx-auto">    
         <div class="sm:grid sm:grid-cols-3 mb-3 md:gap-3 snap-x overflow-x-scroll md:overflow-hidden snap-mandatory w-full mx:auto md:p-3">
