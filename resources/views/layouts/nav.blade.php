ca7a79b97d6102a37e975dd2ac20c6427fe8894c
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-07 20:10:30 -0700

    Update Navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,9 +1,9 @@
 <!-- Navigation -->
-<nav class="xs:bg-blue-700 sm:bg-gray-100 border-gray-200 dark:bg-gray-900 fixed w-full top-0 z-50">
+<nav class="bg-blue-700 border-gray-200 dark:bg-gray-900 fixed w-full top-0 z-50">
     <div class="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://www.jeffreyzeller.com" class="flex items-center space-x-3 rtl:space-x-reverse">
             <!-- <img src="#" class="h-8" alt="Jeffrey Zeller Logo" /> -->
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-mono  xs:text-white sm:text-blue-700">jeffreyzeller.com</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white font-mono text-white">jeffreyzeller.com</span>
         </a>
         <!-- Toggle Button -->
         <button id="nav-toggle" type="button"
@@ -19,7 +19,7 @@
           </svg>
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 shadow-inner md:flex-row md:space-x-1 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray200/10 dark:bg-gray-800 dark:border-gray-700">
+        <ul class="font-medium flex flex-col p-2 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-200 shadow-inner shadow-gray-400 md:flex-row md:space-x-1 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray200/10 dark:bg-gray-800 dark:border-gray-700">
             <li class="px-3 py-2">
             <a href="#home"  class="block text-neutral-700 rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
             </li>
