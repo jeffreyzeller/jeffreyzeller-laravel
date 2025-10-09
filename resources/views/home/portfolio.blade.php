0c99fdf308256eb98c777da16110070849b8e7e7
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-08 17:17:57 -0700

    update portfolio

--- resources/views/home/portfolio.blade.php
+++ resources/views/home/portfolio.blade.php
@@ -1,12 +1,12 @@
 <section id="portfolio" class="w-full text-center content-center bg-blue-700 dark:bg-gray-800 snap-start min-h-[calc(100vh-300px)] pt-28">
     <h2 class="text-4xl text-white dark:text-gray-100 font-bold mb-5">My Recent Work</h2>         
-    
-    <div class="lg:max-w-5xl mx-5 sm:mx-auto">    
+    <p class="text-white dark:text-gray-100">Here’s what I build — custom web apps, dashboards & client portals. Below are my most recent and strongest projects.</p>
+    <div class="lg:max-w-5xl mx-5 mt-10 sm:mx-auto">    
         <div class="sm:grid sm:grid-cols-3 mb-3 md:gap-3 snap-x overflow-x-scroll md:overflow-hidden snap-mandatory w-full mx:auto md:p-3">
             
             @foreach($portfolio as $result)
 
-            <div class="relative bg-white dark:bg-gray-900 rounded-2xl overflow-hidden 
+            <div class="relative group bg-white dark:bg-gray-900 rounded-2xl overflow-hidden 
                 border border-slate-200 dark:border-gray-700 h-100
                 hover:outline-blue-200 dark:hover:outline-gray-700 hover:outline-2 hover:outline-offset-4
                 shadow-sm hover:shadow-white/20 dark:hover:shadow-gray-500/20 transition-all duration-300 hover:-translate-y-1">
@@ -15,8 +15,8 @@
                 <div class="absolute inset-0 bg-gradient-to-t from-blue-900/80 via-blue-900/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
                 
                 <div class="absolute bottom-0 p-6 w-full">
-                    <h3 class="text-xl font-semibold text-white">{{ $result->title }}</h3>
-                    
+                    <h3 class="text-xl font-semibold text-gray-200">{{ $result->title }}</h3>
+                    <p class="text-gray-500 dark:text-gray-200">{{ $result->description }}</p>
                     <p class="text-slate-300 text-sm mt-1">
                         @foreach($result->tags as $tag)
                             <span class="px-2 py-1 bg-slate-200 dark:bg-slate-800 text-xs text-slate-700 dark:text-slate-300 rounded">{{ $tag }}</span>
@@ -29,6 +29,7 @@
                 </div>
             </div>
             <br class="sm-hidden" />
+
             @endforeach
             
         </div>
