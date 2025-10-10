e1097c09419066738bdcab7435e9ee5b98474529
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-10 14:32:12 -0700

    update background. final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html class="scroll-smooth overflow-y-scroll md:snap-y md:snap-mandatory">
+<html class="dark scroll-smooth overflow-y-scroll md:snap-y md:snap-mandatory">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
