37f8d40e0f8c526ac0cc135b92e949f0b390f7ac
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-09 21:56:20 -0700

    update tailwind to use vite. now image loading will be changed for static images no longer bg-[url()] on tailwind side of things.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  darkMode: 'class',
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+  ],
+  theme: {
+    extend: {
+        backgroundImage: {
+        },
+    },
+  },
+  plugins: [],
+}
