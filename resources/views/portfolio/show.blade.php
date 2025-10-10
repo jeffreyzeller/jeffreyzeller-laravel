dd6d7c9496dac43e7b32a2c777237df3baaee53d
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-09 18:50:40 -0700

    update portfolio

--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -6,20 +6,24 @@
     <div class="flex relative max-w-5xl mx-auto text-gray-900 dark:text-white">
         <div>
             <img class="w-full rounded-t-lg sm:rounded sm:shadow-lg" src="{{ asset('storage/' . $portfolio->image) }}">
-            <div class="font-bold rounded-b text-left pt-5">
-                @foreach($portfolio->tags as $tag)
-                            <span class="inline-block px-2 py-1 mb-1 bg-slate-200 dark:bg-slate-700 text-xs text-slate-700 dark:text-slate-300 rounded">{{ $tag }}</span>
-                @endforeach
-            </div>
+            
         </div>
         
         <div class="p-5">
             <a href="{{ $portfolio->url }}"><x-heroicon-s-link class="float-right inline w-4 mr-1" /></a>
             <h1 class="text-xl font-semibold text-gray-900 dark:text-white">{{ $portfolio->title }}</h3>
             
+            <div class="font-bold rounded-b text-left pt-5">
+                @foreach($portfolio->tags as $tag)
+                            <span class="inline-block px-2 py-1 mb-1 bg-slate-200 dark:bg-slate-700 text-xs text-slate-700 dark:text-slate-300 rounded">{{ $tag }}</span>
+                @endforeach
+            </div>
+            
+            
             <div class="py-5 list-disc">
                 {!! str($portfolio->description)->sanitizeHtml() !!}
             </div>
+        
         </div>
     </div>
 </section>
