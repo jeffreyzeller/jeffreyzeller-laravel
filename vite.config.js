a40403569eab41405dc1b4d618642489d0f7aad6
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-09 23:42:58 -0700

    remove image compression tools

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,12 @@ import {
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
-// Imagemin
-import viteImagemin from '@vheemstra/vite-plugin-imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
-import imageminWebp from 'imagemin-webp';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }),
-        viteImagemin({
-            plugins: {
-                jpg: imageminMozjpeg({ quality: 80 }),
-            },
-            makeWebp: {
-                plugins: {
-                    jpg: imageminWebp({ quality: 80 }),
-                },
-            },
         }),
         tailwindcss(),
     ],
