2f588ebf3111bcdab28890082b3b8d09e7365a12
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-10 18:31:42 -0700

    update footer for mobile

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="w-full text-center content-center bg-white/80 dark:bg-slate-950/80 snap-start h-25 lg:h-75 bg-wave-flipped bg-cover bg-no-repeat bg-top pt-75">
+<footer class="w-full text-center content-center bg-white/80 dark:bg-slate-950/80 snap-start h-25 lg:h-75 bg-wave-flipped bg-cover bg-no-repeat bg-top">
     <div class="absolute w-[100%] bottom-0 flex content-center">
         <div class="w-[100%] mx-auto text-left text-xl text-gray-800 dark:text-gray-500 p-5">
             &copy; jeffreyzeller.com
