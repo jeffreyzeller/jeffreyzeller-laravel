a39a8d91a2a3e503d8ac1f1480e25f66caf00dec
Jeffrey Zeller <accounts@jeffreyzeller.com>
2025-10-11 01:43:01 -0700

    update logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-blue-500 dark:bg-gray-800  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="/images/logo.png" class="h-8" alt="Flowbite Logo">
+  <a href="{{ env('APP_URL') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="/images/logo.svg" class="h-8" alt="Flowbite Logo">
       <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Jeffrey Zeller</span>
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
